d64230b9d6bd72fadd113acd1e0b59617a157bba
pbmk-lab <jurijs.veskins@iterum.lv>
2026-03-19 08:17:27 +0200

    Update app.py

--- app.py
+++ app.py
@@ -46,16 +46,22 @@ df["month"] = df["start_date"].dt.to_period("M").astype(str)
 LINE_MAPPING = {
     "01. 1 LĪNIJA": ["1 LĪNIJA"],
     "02. 2 LĪNIJA": ["2 LĪNIJA"],
-    "03. 3 LĪNIJA RIPLAST": ["RIPLAST"],
+    "03. 3 LĪNIJA RIPLAST": ["RIPLAST", "3 LĪNIJA"],
     "04. 4 LĪNIJA": ["4 LĪNIJA"],
     "05. SMALKUMU LĪNIJA SM1": ["SM1"],
     "06. SMALKUMU LĪNIJA SM2 (VIOLIA)": ["SM2", "VIOLIA"],
     "07. SMALKUMU LĪNIJA SM 3": ["SM 3", "SM3"],
     "08. SMALKUMU LĪNIJA (KORKU)": ["KORKU"],
-    "09. TOMRA KORKU LĪNIJA": ["TOMRA"],
+    "09. TOMRA KORKU LĪNIJA": ["TOMRA KORKU", "TOMRA"],
     "10. WIPA": ["WIPA"],
-    "11. ĶĪPU PRESE": ["PRESE"],
-    "12. KOMPRESORI": ["KOMPRESOR"]
+    "11. ĶĪPU PRESE": ["PRESE", "ĶĪPU PRESE"],
+    "12. KOMPRESORI": ["KOMPRESOR", "KOMPRESORI"],
+    "13. STARLINGER NEW": ["STARLINGER NEW"],
+    "14. STARLINGER OLD": ["STARLINGER OLD"],
+    "15. Steiner Meyer": ["STEINER MEYER", "STEINER", "MEYER"],
+    "16. NTomra": ["NTOMRA", "N TOMRA"],
+    "17. LUMSU LĪNIJA": ["LUMSU LĪNIJA", "LUMSU"],
+    "18. ATTĪRĪŠANAS IEKĀRTAS": ["ATTĪRĪŠANAS IEKĀRTAS"]
 }
 
 def extract_line(location):
